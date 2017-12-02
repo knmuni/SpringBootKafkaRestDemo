34affefd2fe7232dd06cf0b4844f24a8754230f6
knmuni <knmuny@gmail.com>
2017-12-02 14:39:24 -0500

    This SpringBootKafkaRestDemo

---
+++ src/main/java/com/test/kafka/SpringBootKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.test.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(
+				new Object[] { SpringBootKafkaApplication.class }, args);
+	}
+}
---
+++ src/main/java/com/test/kafka/service/KafkaSender.java
@@ -0,0 +1,19 @@
+package com.test.kafka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaSender {
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	String kafkaTopic = "kafka_test_topic";
+	
+	public void send(String data) {
+	    
+	    kafkaTemplate.send(kafkaTopic, data);
+	}
+}
